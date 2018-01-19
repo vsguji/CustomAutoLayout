35b20af21339f5cc7381cef21869f52af1878880
vsguji <vsguji_2012@sian.com>
2018-01-19 21:39:37 +0800

    Initial Commit

---
+++ CustomerAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomerAutoLayout
+//
+//  Created by lipeng on 2018/1/19.
+//  Copyright © 2018年 lipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomerAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomerAutoLayout
+//
+//  Created by lipeng on 2018/1/19.
+//  Copyright © 2018年 lipeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
